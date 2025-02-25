cca1e827e71d988aa5507696b0b654a1e344ccbe
jia-jjin <lewjjn@gmail.com>
2025-02-25 15:23:17 +0800

    add search by operator and cancel request (backend)

--- server/app/routes/license.py
+++ server/app/routes/license.py
@@ -60,6 +60,26 @@ def get_all_licenses():
 
     return jsonify(licenses_json), 200
 
+@license_bp.route('/operator', methods=['GET'])
+@jwt_required()
+def get_all_licenses_by_operator():
+    operator_id = get_jwt_identity()
+
+    licenses = License.objects(operator=operator_id).all()
+
+    licenses_json = []
+    for license in licenses:
+        license_dict = license.to_mongo().to_dict()
+
+        license_dict["_id"] = str(license_dict["_id"]) 
+        license_dict["operator"] = str(license_dict["operator"])  
+
+        license_dict["features"] = [{"name": feature.name} for feature in license.features]
+
+        licenses_json.append(license_dict)
+
+    return jsonify(licenses_json), 200
+
 @license_bp.route('/<license_id>/status', methods=['PUT'])
 @jwt_required()
 def update_license_status(license_id):
@@ -77,4 +97,23 @@ def update_license_status(license_id):
     license.approved = data.get("status")
     license.save()
 
-    return jsonify({"status": license.approved, "license_id": str(license.id)})
+    return jsonify({"status": license.approved.value, "license_id": str(license.id)})
+
+@license_bp.route('/<license_id>/cancel', methods=['DELETE'])
+@jwt_required()
+def cancel_license(license_id):
+    operator_id = get_jwt_identity()
+
+    license = License.objects(id=license_id).first()
+    if not license:
+        return jsonify({"msg": "License not found"}), 404
+    
+    license_dict = license.to_mongo().to_dict()
+    license_dict["operator"] = str(license_dict["operator"])  
+
+    if license_dict["operator"] != operator_id:
+        return jsonify({"msg": "Only the owner of the license request can cancel it."}), 403
+
+    license.delete()
+
+    return jsonify({"status": "cancelled", "license_id": str(license.id)})
